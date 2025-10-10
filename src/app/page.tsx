43e1d6fdc6fe8ee81ed1ff64a3377d16205307ec
Bikalpa Shrestha <sthabikalpa0712@gmail.com>
2025-10-10 18:41:34 +0545

    UI changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/components/hero-section'
 import { FeaturesSection } from '@/components/features-section'
-import { BenefitsSection } from '@/components/benefits-section'
 import { TestimonialsSection } from '@/components/testimonials-section'
 import { Pricing } from '@/components/ui/pricing-cards'
 
@@ -10,9 +9,6 @@ export default function Home() {
       <HeroSection />
       <section id="features" data-animate="up">
         <FeaturesSection />
-      </section>
-      <section data-animate="up">
-        <BenefitsSection />
       </section>
       <section id="testimonials" data-animate="up">
         <TestimonialsSection />
--- src/components/benefits-section.tsx
+++
@@ -1,5 +0,0 @@
-import Casestudies from "@/components/ui/case-studies"
-
-export function BenefitsSection() {
-  return <Casestudies />
-}
